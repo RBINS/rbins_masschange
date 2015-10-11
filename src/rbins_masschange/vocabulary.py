b5a7eec5bb5e558236b5ed99194ab8c1d7dc4328
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-10-11 11:41:47 +0200

    contributors voc

--- src/rbins_masschange/vocabulary.py
+++ src/rbins_masschange/vocabulary.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-__docformat__ = 'restructuredtext en'
-
-
+from binascii import b2a_qp
 from zope import interface, component
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from zope.schema.interfaces import IVocabularyFactory, IContextSourceBinder
 
+from Products.CMFPlone.utils import safe_unicode, getToolByName
+from zope.site.hooks import getSite
 
 from plone.i18n.normalizer.base import baseNormalize
 
@@ -42,4 +42,35 @@ class LocalKeywordFactory(object):
         return SimpleVocabulary(values)
 
 LocalSubjectsVocabulary = LocalKeywordFactory('Subject')
+
+
+class ContributorsVocabularyFactory(object):
+    """
+    """
+    interface.implements(IVocabularyFactory)
+
+    def __call__(self, context, query=None):
+        site = getSite()
+        self.catalog = getToolByName(site, "portal_catalog", None)
+        if self.catalog is None:
+            return SimpleVocabulary([])
+        index = self.catalog._catalog.getIndex('Subject')
+
+        def safe_encode(term):
+            if isinstance(term, unicode):
+                # no need to use portal encoding for transitional encoding from
+                # unicode to ascii. utf-8 should be fine.
+                term = term.encode('utf-8')
+            return term
+
+        # Vocabulary term tokens *must* be 7 bit values, titles *must* be
+        # unicode
+        items = [
+            SimpleTerm(i, b2a_qp(safe_encode(i)), safe_unicode(i))
+            for i in index._index
+            if query is None or safe_encode(query) in safe_encode(i)
+        ]
+        return SimpleVocabulary(items)
+
+ContributorsVocabulary = ContributorsVocabularyFactory()
 # vim:set et sts=4 ts=4 tw=80:
